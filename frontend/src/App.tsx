b82f097b73360b2df5c9c32dcd2095f587e69e4f
MargotRasamy <rasamymargot@gmail.com>
2022-09-27 15:37:05 +0200

    Tested an API request and added axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const getMovieService = () => {
+    axios.get(`http://127.0.0.1:3000/api/v1/movies`).then(res => {
+      console.log(res.data)
+    })
+  }
+
   return (
     <div className="App">
       hello
+      <button type="button" onClick={() => getMovieService ()}>
+        get movie service
+      </button>
     </div>
   );
 }
