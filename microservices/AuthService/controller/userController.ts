1c1f3e7faa265c2f5d06d9ad275a4e49e68e021d
MargotRasamy <rasamymargot@gmail.com>
2022-10-04 16:07:47 +0200

    Login method working but needs JWT token

--- microservices/AuthService/controller/userController.ts
+++ microservices/AuthService/controller/userController.ts
@@ -2,12 +2,17 @@ import express from "express";
 import { hash, compare } from 'bcrypt'
 import { User } from "../database/models/User";
 import UserTable from "../database/schemas/UserSchema";
-import mongoose, { MongooseError } from 'mongoose';
+import mongoose from 'mongoose';
+const { validationResult } = require('express-validator');
 
 // --------------- CREATE USER
 export const createUser = async (request:express.Request, response:express.Response) : Promise<express.Response> => {
-    console.log("New user is "  + JSON.stringify(request.body));
-    
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(401).json({ errors: errors.array() });
+    }
+
     const hashedPassword = await hash(request.body.password, 10);
 
     // New user
@@ -48,6 +53,49 @@ export const createUser = async (request:express.Request, response:express.Respo
                 msg: `Your email or username has already been used. Please retry with another one.`
             });
         }
+    }
+}
+
+// --------------- LOGIN METHOD
+export const login = async (request:express.Request, response:express.Response) : Promise<express.Response> => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(401).json({ errors: errors.array() });
+    }
+
+    if (!request.body.username && !request.body.email) {
+        return response.status(401).json({
+            msg: 'You did not fill your username or email'
+        });
+    }
+
+    const userToAuthenticate = !!request.body.username ? await UserTable.findOne({ username: request.body.username }) : await UserTable.findOne({ email: request.body.email });
+
+    try {
+        if (!!userToAuthenticate) {
+
+            const isPasswordCorrect = await compare(request.body.password, userToAuthenticate.hashed_password);
+            
+            if (!!isPasswordCorrect) {
+                return response.status(200).json({
+                    msg: 'You are now logged in !'
+                });
+            } else {
+                return response.status(401).json({
+                    msg: 'Your password is incorrect'
+                });
+            }
+            
+        } else {
+            return response.status(200).json({
+                msg: 'Username, email or password incorrect.'
+            });
+        } 
+    } catch (e) {
+        return response.status(500).json({
+            msg: 'Unknown server error triggered. Please try again.'
+        });
     }
 }
 
--- microservices/AuthService/router/userRouter.ts
+++ microservices/AuthService/router/userRouter.ts
@@ -1,12 +1,56 @@
 import express from 'express';
-import { createUser, getUsers } from "../controller/userController";
+import { createUser, getUsers, login } from "../controller/userController";
+const { check, oneOf } = require('express-validator');
+
+// Express validators
 
 const userRouter:express.Router = express.Router();
 
 // Users
-userRouter.post('/', createUser);
+
+const registerValidationRules = [
+    check('password')
+        .notEmpty()
+        .withMessage('Password not filled')
+        .isLength({ min: 5 })
+        .withMessage('Must be at least 5 chars long')
+        .matches(/\d/)
+        .withMessage('Must contain a number')
+        .matches(/[^\w\s]/)
+        .withMessage('Must contain a special character')
+        .matches(/[A-Z]/)
+        .withMessage('Must contain an uppercase letter')
+        .matches(/[a-z]/)
+        .withMessage('Must contain a lowercase letter'),
+    check('email').notEmpty()
+        .withMessage('Email not filled').isEmail().withMessage('Not an email'),
+    check('username').notEmpty()
+        .withMessage('Username not filled')
+]
+
+const emailOrUsernameRules = [
+    check('email')
+        .notEmpty()
+        .withMessage('Email not filled')
+        .isEmail()
+        .withMessage('Not an email'),
+    check('username')
+        .notEmpty()
+        .withMessage('Username not filled')
+]
+
+const loginValidationRules = [
+    check('password')
+        .notEmpty()
+        .withMessage('Password not filled')
+]
+
+// Register user
+userRouter.post('/', registerValidationRules, createUser);
 
 userRouter.get('/', getUsers);
+
+userRouter.post('/login', oneOf(emailOrUsernameRules), loginValidationRules, login);
 
 
 export default userRouter;
