022928af465de5b629222f8ca8cdedab2f9e58b2
salamisodikiolawale <salamisodikiolawale@gmail.com>
2022-10-29 18:52:25 +0200

    refont project for ci cd

--- frontend/src/components/BurgerMenu.tsx
+++
@@ -1,9 +0,0 @@
-import "../styles/components/burger-menu.scss";
-
-const BurgerMenu = ({onClickAction} : any) => {
-    return (
-        <button type="button" onClick={onClickAction} className="burger-menu">&#9776;</button>
-    )
-}
-
-export default BurgerMenu;
