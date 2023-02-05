84d7abf3995612cf66bad66062c75eae90c77d56
StardustRL <romain.liu2000@gmail.com>
2023-02-05 21:11:43 +0100

    Remove comments in apiRouter.ts

--- microservices/SearchService/router/apiRouter.ts
+++ microservices/SearchService/router/apiRouter.ts
@@ -12,44 +12,4 @@ const apiRouter:express.Router = express.Router();
  */
 apiRouter.get('/search',getFilteredListOfMovies);
 
-/*
-    Usage: Update a Movie
-    URL : http://127.0.0.1:3000/api/v1/movies/:movieId
-    Method : PUT
-    Fields: name, image, comment
-    Access: Public
- */
-//apiRouter.put('/movies/:movieId', updateMovie);
-
-/*
-    Usage: Delete a Movie
-    URL : http://127.0.0.1:3000/api/v1/movies/:movieId
-    Method : DELETE
-    Fields: name, image, comment
-    Access: Public
- */
-//apiRouter.delete('/search/title/:movieTitle', deleteMovie);
-//apiRouter.delete('/search/type/movieType', deleteMovie);
-
-//apiRouter.delete('/movies/:movieId', deleteMovie);
-
-/*
-    Usage: Get all the movies
-    URL : http://127.0.0.1:3000/api/v1/movies
-    Method : GET
-    Fields:  name, image, comment
-    Access: Public
- */
-//apiRouter.get('/movies', getMovies);
-
-/*
-    Usage: Get a Single product
-    URL : http://127.0.0.1:3000/api/v1/movies/:movieId
-    Method : GET
-    Fields: name, image, price, qty, info
-    Access: Public
- */
-
-//apiRouter.get('/movies/:movieId', getMovie);
-
 export default apiRouter;
